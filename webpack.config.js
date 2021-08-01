24288c7783ee7f465105a6c57ff35e8241b10af3
royster <rcantu786@gmail.com>
2021-07-31 19:35:41 -0500

    modularized code

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require("path");
+
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static"
+  })
+  ],
+  mode: "development"
+};
